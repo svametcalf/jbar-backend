38d50157878bf9583240d8f5c4d42107a7e7c42d
Schuyler Metcalf <svametcalf@gmail.com>
2017-04-16 17:17:35 -0800

    added shell nginx configuration

---
+++ proxy/run.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+NGINX_CONF_DIR=/etc/nginx/conf.d/
+TEMPLATE_DIR=templates/
+
+rename_template(){
+    path=$1
+    name=${path##*/}
+    name=${name%.template.conf}
+    renamed_filename="$NGINX_CONF_DIR$name.conf"
+    echo $renamed_filename
+}
+
+sub_template(){
+    file_to_sub=$1
+
+    rename_template $file_to_sub
+
+    echo "subsituting $file_to_sub and copying to new file $renamed_filename"
+    envsubst < $file_to_sub > $renamed_filename
+    cat $renamed_filename
+}
+
+sub_all_templates(){
+    for file in $TEMPLATE_DIR*.template.conf; do
+        sub_template $file
+    done
+}
+
+sub_all_templates && \
+    nginx -g 'daemon off;'
